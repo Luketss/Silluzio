97c30fd5c4e45cae9493b10e11861e40e7a2725e
Lucas Silluzio <lucassilluzio@gmail.com>
2023-02-25 20:29:31 -0300

    bot blaze code

--- Python/Bot/Blaze/main.py
+++ Python/Bot/Blaze/main.py
@@ -1,36 +1,26 @@
-import requests
+import time
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-import time
-import chromedriver_binary
 
-a = []
-# options = webdriver.ChromeOptions()
-# options.add_argument("--headless")
 
-# driver = webdriver.Chrome(chrome_options=options)
+valores_passados = []
+
 driver = webdriver.Chrome()
 driver.get("https://blaze.com/pt/games/crash")
 
-time.sleep(5)
-
-try:
-    element = WebDriverWait(driver, 15).until(
-        EC.presence_of_element_located((By.XPATH, "//div[@class='crash-previous']"))
-    )
-except:
-    pass
+time.sleep(4)
 
 previous = driver.find_element(By.XPATH, "//div[@class='crash-previous']")
-previous_values = previous.find_element(By.XPATH, "//div[@class='entries']")
 
-span_list = previous_values.find_elements(By.TAG_NAME, "span")
+p = previous.find_element(By.XPATH, "//div[@class='entries']")
 
-for v in span_list:
-    a.append(v.text)
+values = p.find_elements(By.TAG_NAME, "span")
+
+for v in values:
+    if v.text != "":
+        valores_passados.append(v.text)
+
+print(valores_passados[::-1])
 
 driver.quit()
-print(a[::-1])
